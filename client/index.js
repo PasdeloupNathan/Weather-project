986fb0a708844798992843fed9c102d0178c481a
trak-dev <yann.picaud@gmail.com>
2021-10-12 16:04:33 +0200

    front connect to back

--- client/index.js
+++ client/index.js
@@ -6,5 +6,9 @@ var app = new Vue({
     mounted: function mounted() {
         var vm = this;
         vm.test = "all right"
+        axios.get("http://localhost:5000/").then(
+            res => {
+                console.log(res.data);
+            })
     }
 })
