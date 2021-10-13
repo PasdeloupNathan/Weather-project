00a70768dfbf37c754b3340c39bffbe578ea2e31
trak-dev <yann.picaud@gmail.com>
2021-10-14 00:00:43 +0200

    latest

--- client/test/index.js
+++ client/test/index.js
@@ -5,7 +5,7 @@ var app = new Vue({
     },
     mounted: function mounted() {
         var vm = this;
-        vm.test = "all right"
+        vm.test = "all right";
         axios.get("http://localhost:5000/").then(
             res => {
                 console.log(res.data);
